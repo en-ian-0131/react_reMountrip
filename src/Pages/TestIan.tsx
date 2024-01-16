7f2fca9a2da02b78a3c4a450f85a233a3dcd5f45
en-ian-0131 <DaiMingZer@gmail.com>
2024-01-16 20:14:01 +0800

    test Blob

--- src/Pages/TestIan.tsx
+++ src/Pages/TestIan.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+export default function TestIan() {
+  const numberArray = [72, 101, 108, 108, 111];
+  const uint8Array = new Uint8Array(numberArray);
+  console.log("uint8Array:", uint8Array);
 
-function TestIan() {
+  const textBlob2 = new Blob([uint8Array], {
+    type: "application/octet-stream",
+  });
+  const BlobURL = URL.createObjectURL(textBlob2);
+
+  const handleClick = () => {
+    const ALink = document.createElement("a");
+    ALink.href = BlobURL;
+    ALink.download = "text.xls";
+    ALink.click();
+  };
+
   return (
-    <div>TestIan</div>
-  )
+    <div>
+      <button
+        onClick={() => {
+          handleClick();
+        }}
+      >
+        Download
+      </button>
+    </div>
+  );
 }
-
-export default TestIan
