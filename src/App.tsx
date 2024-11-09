55736a549e99a60d8904e01e154b93807e6c916a
en-ian-0131 <DaiMingZer@gmail.com>
2024-11-09 17:33:21 +0800

    整理專案

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 //Mountrip
 import MountripApp from "./Pages/MountripPages/MountripApp";
 //Test uint8Array Download
-import TestUint8ArrayDownload from "./Pages/MountripPages/TestUint8ArrayDownload";
+import TestUint8ArrayDownload from "./Pages/Download/TestUint8ArrayDownload";
 
 function App() {
   return (
